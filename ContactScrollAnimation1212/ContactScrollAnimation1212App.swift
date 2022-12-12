84328834b27cc1ca29c4f4f188bcc99e160ec0f6
张亚飞 <672233580@qq.com>
2022-12-12 15:53:31 +0800

    Initial Commit

---
+++ ContactScrollAnimation1212/ContactScrollAnimation1212App.swift
@@ -0,0 +1,17 @@
+//
+//  ContactScrollAnimation1212App.swift
+//  ContactScrollAnimation1212
+//
+//  Created by 张亚飞 on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct ContactScrollAnimation1212App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
